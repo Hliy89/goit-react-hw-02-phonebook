6af1c3cce07e85c58715c2d464db0acf9bbdf8f9
Yaroslav Hliy <hliy.yaroslav@gmail.com>
2021-04-22 20:06:04 +0300

    установил npx, prettierrc, и т. п.

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import style from './App.module.css';
+
+function App() {
+  return (
+    <div className={style.App}>
+      <p>Hi all!!!</p>
+    </div>
+  );
+}
+
+export default App;
